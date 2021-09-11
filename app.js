7f443ed2283df74443fa060a6ba90aa84d4301f3
mohammad-a-hossain <adam.eve007@gmail.com>
2021-09-11 14:10:44 +0600

    code clear

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // using database mongoose PART- 2
-const express =require('express')
+/* const express =require('express')
 const morgan =require('morgan')
 const mongoose = require('mongoose')
 const router =require('./contactRout')
@@ -19,7 +19,7 @@ app.get('*',(req,res)=>{
     res.json({
         message:"welcome to out site "
     })
-})
+}) */
 
 
 
@@ -43,41 +43,31 @@ app.get('*',(req,res)=>{
 // without database using
 
 
-// const { urlencoded } = require('express')
-// const express =require('express')
-// const morgan =require('morgan')
-// const userRoute =require('./contactRout')
 
 
-// const app =express()
 
-// app.use(morgan('dev'))
-// app.use(express.urlencoded({extended:true}))
-// app.use(express.json())
+const { urlencoded } = require('express')
+const express =require('express')
+const morgan =require('morgan')
+const userRoute =require('./contactRout')
 
+const app =express()
 
-// const { urlencoded } = require('express')
-// const express =require('express')
-// const morgan =require('morgan')
-// const userRoute =require('./contactRout')
-
-// const app =express()
-
-// app.use(morgan('dev'))
-// app.use(express.urlencoded({extended:true}))
-// app.use(express.json())
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 
-// app.use('/users',userRoute)
+app.use('/users',userRoute)
 
-// app.get('*',(req,res)=>{
-//     res.send('<h2>page not found 404 error</h2>')
-// })
+app.get('*',(req,res)=>{
+    res.send('<h2>page not found 404 error</h2>')
+})
 
-// const PORT= process.env.PORT || 4000
-// app.listen(PORT,()=>{
-// console.log(`the app in running on port ${PORT}`)
-// })
+const PORT= process.env.PORT || 4000
+app.listen(PORT,()=>{
+console.log(`the app in running on port ${PORT}`)
+})
 
 
 
